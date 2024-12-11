580fa77779c26956d8a76a70505b2d9bd728412e
Doug Wilson <d@dkw.io>
2024-12-11 10:24:54 -0800

    Added extension methods

---
+++ framework/src/DKW.Abp.Microservices/DKW/Abp/Microservices/ServiceConfigurationContextExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StackExchange.Redis;
+using Volo.Abp.Modularity;
+
+using static DKW.Abp.Microservices.ConfigKeys;
+
+namespace DKW.Abp.Microservices;
+
+public static class ServiceConfigurationContextExtensions
+{
+    public static void ConfigureDataProtection(this ServiceConfigurationContext context, String name)
+    {
+        var configuration = context.Services.GetConfiguration();
+        var hostingEnvironment = context.Services.GetHostingEnvironment();
+
+        var dataProtectionBuilder = context.Services.AddDataProtection().SetApplicationName(name);
+        if (!hostingEnvironment.IsDevelopment())
+        {
+            var redis = ConnectionMultiplexer.Connect(configuration[RedisConfiguration] ?? "redis");
+            dataProtectionBuilder.PersistKeysToStackExchangeRedis(redis, DataProtectionKey);
+        }
+    }
+}
