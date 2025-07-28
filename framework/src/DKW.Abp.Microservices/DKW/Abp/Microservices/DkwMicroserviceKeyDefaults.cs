8615df3cf6814b9abe912ef532e119fb2e3057e6
Doug Wilson <d@dkw.io>
2025-07-27 22:27:37 -0700

    Namespace consistency

--- framework/src/DKW.Abp.Microservices/DKW/Abp/Microservices/DkwMicroserviceKeyDefaults.cs
+++ framework/src/DKW.Abp.Microservices/DKW/Abp/Microservices/DkwMicroserviceKeyDefaults.cs
@@ -5,8 +5,8 @@ public static class DkwMicroserviceKeyDefaults
     public const String AspireRedis = "ConnectionStrings:Redis";
     public const String Authority = "AuthServer:Authority";
     public const String CorsOrigins = "App:CorsOrigins";
-    public const String DataProtectionKey = "DKW-Protection-Keys";
-    public const String DistributedCacheKeyPrefix = "DKW:";
+    public const String DataProtectionKey = "Dkw-Protection-Keys";
+    public const String DistributedCacheKeyPrefix = "Dkw:";
     public const String RedisConfiguration = "Redis:Configuration";
     public const String RequireHttpsMetadata = "AuthServer:RequireHttpsMetadata";
 }
--- push.ps1
+++ push.ps1
@@ -1,4 +1,4 @@
 Remove-Item -Path .\artifacts\ -Recurse -Force -ErrorAction SilentlyContinue
-dotnet build .\DKW.Abp.sln --configuration Release --no-restore
+dotnet build .\Dkw.Abp.sln --configuration Release --no-restore
 dotnet test --configuration Release --no-restore --no-build --verbosity normal
 nuget push .\artifacts\*.nupkg -Source 'https://api.nuget.org/v3/index.json' -ApiKey $env:NUGET_API_KEY
